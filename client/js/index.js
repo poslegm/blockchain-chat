188e1e7614def5b9f73973bea7b3654458df3ce5
poslegm <poslegm@gmail.com>
2016-07-08 17:09:09 +0300

    Добавлен черновой вариант графического интерфейса

---
+++ client/js/index.js
@@ -0,0 +1,65 @@
+(function() {
+  var NYLM, claerResizeScroll, conf, getRandomInt, insertI, lol;
+
+  conf = {
+    cursorcolor: "#696c75",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  lol = {
+    cursorcolor: "#cdd2d6",
+    cursorwidth: "4px",
+    cursorborder: "none"
+  };
+
+  getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  claerResizeScroll = function() {
+    $("#texxt").val("");
+    $(".messages").getNiceScroll(0).resize();
+    return $(".messages").getNiceScroll(0).doScrollTop(999999, 999);
+  };
+
+  insertI = function() {
+    var innerText, otvet;
+    innerText = $.trim($("#texxt").val());
+    if (innerText !== "") {
+      $(".messages").append(
+            "<li class=\"i\"><div class=\"head\"><span class=\"time\">" +
+            (new Date().getHours()) + ":" + (new Date().getMinutes()) +
+            ", Today</span><span class=\"name\"> Букер</span></div><div class=\"message\">" +
+            innerText + "</div></li>"
+      );
+      claerResizeScroll();
+
+      return otvet = setInterval(function() {
+        $(".messages").append(
+            "<li class=\"friend-with-a-SVAGina\"><div class=\"head\"><span class=\"name\">VS94SKI  </span><span class=\"time\">" +
+            (new Date().getHours()) + ":" + (new Date().getMinutes()) +
+            ", Today</span></div><div class=\"message\">" +
+            "ЧПЕК" + "</div></li>"
+        );
+        claerResizeScroll();
+        return clearInterval(otvet);
+      }, getRandomInt(2500, 500));
+    }
+  };
+
+  $(document).ready(function() {
+    $(".list-friends").niceScroll(conf);
+    $(".messages").niceScroll(lol);
+    $("#texxt").keypress(function(e) {
+      if (e.keyCode === 13) {
+        insertI();
+        return false;
+      }
+    });
+    return $(".send").click(function() {
+      return insertI();
+    });
+  });
+
+}).call(this);
